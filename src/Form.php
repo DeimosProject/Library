e1b7d3298b21e89ee390bd6dfed5fbe213108a0c
REZ1DENT3 <maksim.babichev95@gmail.com>
2015-04-16 16:55:17 +0300

    + Добавлено кеширование(на уровне запроса...) + добавлена функция возврата телефона

--- src/Form.php
+++ src/Form.php
@@ -3,15 +3,33 @@
 class Form
 {
 
+    protected static $_cache = array();
+
+    private static $regx_phone = '/[^\d]/i';
+
     /**
      * @param string $string
      * @return bool
      */
     public static function is_phone($string)
     {
-        $string = preg_replace('/[^\d]/i', '', $string);
-        $length = strlen($string);
+        if (isset(self::$_cache['phone'][$string]))
+            return self::$_cache['phone'][$string];
+        self::$_cache['phone'][$string] = preg_replace(self::$regx_phone, '', $string);
+        $length = strlen(self::$_cache['phone'][$string]);
         return $length == 10 || $length == 11;
+    }
+
+    /**
+     * @param string $string
+     * @return string
+     */
+    public static function phone($string)
+    {
+        if (isset(self::$_cache['phone'][$string]))
+            return self::$_cache['phone'][$string];
+        self::$_cache['phone'][$string] = preg_replace(self::$regx_phone, '', $string);
+        return self::$_cache['phone'][$string];
     }
 
 }
--- tests/FormTest.php
+++ tests/FormTest.php
@@ -8,13 +8,6 @@ class FormTest extends PHPUnit_Framework_TestCase
     {
 
         $this->assertTrue(Form::is_phone("+ 7 (918) 382 - 2970"));
-        $this->assertTrue(Form::is_phone("+ 7 (9 18) 382 - 2970"));
-        $this->assertTrue(Form::is_phone("+ 7 (918) 382 - 2970"));
-        $this->assertTrue(Form::is_phone("+ 7 (91    8) 382 - 2970"));
-        $this->assertTrue(Form::is_phone("+ 7 (918) 382 - 2970"));
-        $this->assertTrue(Form::is_phone("+ 7 (91      8)##@***--- 382 - 29###@#$%^&*()70"));
-        $this->assertTrue(Form::is_phone("+ 7 (918@38 - 29qwerty70"));
-
     }
 
     public function testPhoneStrEmpty()
